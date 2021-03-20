d7dd68ccf12c7da2699bd6a7e28faf0c7006e786
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-20 17:29:39 -0300

    test: add initialize chat use case

---
+++ ecosystem/server/src/usecases/initialize-chat/initialize-chat.spec.ts
@@ -0,0 +1,47 @@
+import { InitializeChat } from "@usecases/initialize-chat"
+import { FakeMessagesRepository } from "@usecases/output-ports/repositories"
+import { LoadMessagesError } from "@usecases/errors"
+import { InfraError } from "@usecases/output-ports/errors"
+import { left } from "@shared/either"
+
+interface SutTypes {
+  sut: InitializeChat,
+  fakeMessagesRepository: FakeMessagesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const fakeMessagesRepository = new FakeMessagesRepository();
+  const sut = new InitializeChat(fakeMessagesRepository);
+
+  return {
+    sut,
+    fakeMessagesRepository
+  }
+}
+
+describe("Use Case Initialize Chat Tests", () => {
+  test("should be call adapter method correctly", async () => {
+    const { sut, fakeMessagesRepository } = makeSut();
+
+    const spyAdapter = jest.spyOn(sut, "adapterRepoMessagesInEntityMessages");
+    await sut.init()
+
+    const fakeMessages = await fakeMessagesRepository.retrievMessages()
+    expect(spyAdapter).toHaveBeenCalledWith(fakeMessages.value);
+  })
+
+  test("should be trace error case in initialize chat", async () => {
+    const { sut, fakeMessagesRepository } = makeSut();
+
+    const spyRetrievMessages = jest.spyOn(fakeMessagesRepository, "retrievMessages");
+    spyRetrievMessages.mockImplementation(() => {
+      const res = new InfraError()
+      return new Promise(resolve => resolve(left(res)))
+    })
+
+    const testable = await sut.init();
+
+    expect(testable.isLeft()).toBeTruthy();
+    expect(testable.value).toEqual(new LoadMessagesError(new InfraError().message))
+  })
+})
