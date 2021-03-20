986a0ddd6768ad7d6bdc62c345a11d88cdbbe1c2
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-20 17:29:28 -0300

    feat: add initialize chat use case

---
+++ ecosystem/server/src/usecases/initialize-chat/index.ts
@@ -0,0 +1,1 @@
+export * from "@usecases/initialize-chat/initialize-chat"
---
+++ ecosystem/server/src/usecases/initialize-chat/initialize-chat.ts
@@ -0,0 +1,45 @@
+import { User } from "@entities/user"
+import { Chat } from "@entities/chat"
+import { Message } from "@entities/message"
+import { MessagesRepository, SavedMessage } from "@usecases/output-ports/repositories"
+import { LoadMessagesError } from "@usecases/errors"
+import { Either, left, right } from "@shared/either"
+
+export class InitializeChat {
+  private readonly messagesRepository: MessagesRepository;
+
+  constructor(messagesRepository: MessagesRepository) {
+    this.messagesRepository = messagesRepository
+  }
+
+  async init(): Promise<Either<LoadMessagesError, Chat>> {
+    const savedInRepositoryMessages = await this.messagesRepository.retrievMessages();
+    if (savedInRepositoryMessages.isLeft()) {
+      return left(new LoadMessagesError(savedInRepositoryMessages.value.message))
+    }
+    const adaptedMessages = this.adapterRepoMessagesInEntityMessages(savedInRepositoryMessages.value);
+
+    return right(Chat.bootstrap({
+      messages: adaptedMessages
+    }))
+  }
+
+  adapterRepoMessagesInEntityMessages(repoMessages: Array<SavedMessage>): Array<Message> {
+    const adaptedInterface: Message[] = [];
+
+    for (let i = 0; i < repoMessages.length; i++) {
+      const message = repoMessages[i];
+      const userOrError = User.create({
+        name: message.user.name,
+        username: message.user.username,
+        password: message.user.password
+      });
+      if (userOrError.isRight()) {
+        const messageInstance = Message.create(userOrError.value, message)
+        adaptedInterface.push(messageInstance)
+      }
+    }
+
+    return adaptedInterface;
+  }
+}
