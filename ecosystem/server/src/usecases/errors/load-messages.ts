2f4c2cad6e765b101e99e291ebd934f5d4591e47
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-20 17:27:25 -0300

    feat: add use cases error

---
+++ ecosystem/server/src/usecases/errors/load-messages.ts
@@ -0,0 +1,6 @@
+export class LoadMessagesError extends Error {
+  constructor(cause: string) {
+    super(`Messages could not be loaded.\nCause: ${cause}`)
+    this.name = "LoadMessagesError"
+  }
+}
