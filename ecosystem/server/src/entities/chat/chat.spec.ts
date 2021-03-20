0e4d38bb766c42da2ca3d6709833d59148e3ecac
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-20 12:29:56 -0300

    test: add push message to entity chat bucket

--- ecosystem/server/src/entities/chat/chat.spec.ts
+++ ecosystem/server/src/entities/chat/chat.spec.ts
@@ -1,5 +1,22 @@
 import { Chat } from "@entities/chat"
 import { makeFakeMessage } from "@entities/message"
+import { makeFakeUser } from "@entities/user"
+import { Message } from "@entities/message"
+import { Content } from "@entities/message/values"
+
+const makeFakeChatWithThreeInitialMessages = () => {
+  const { sut } = makeSut();
+
+  const fakeMessage = makeFakeMessage();
+  const fakeMessageBucket = {
+    messages: [fakeMessage, fakeMessage, fakeMessage]
+  }
+
+  return {
+    chat: sut.bootstrap(fakeMessageBucket),
+    fakeMessageBucket
+  }
+}
 
 const makeSut = () => {
   const sut = Chat
@@ -11,14 +28,17 @@ const makeSut = () => {
 
 describe("Entitiy Chat Tests", () => {
   test("should be initialize Chat entity, receiving an Message Bucket", () => {
-    const { sut } = makeSut();
+    const { chat, fakeMessageBucket } = makeFakeChatWithThreeInitialMessages();
+    expect(chat).toEqual(fakeMessageBucket)
+  })
 
-    const fakeMessage = makeFakeMessage();
-    const fakeMessageBucket = {
-      messages: [fakeMessage, fakeMessage, fakeMessage]
-    }
+  test("should be add unique message in Chat instance", () => {
+    const { chat } = makeFakeChatWithThreeInitialMessages();
 
-    const testable = sut.create(fakeMessageBucket)
-    expect(testable).toEqual(fakeMessageBucket)
+    const fakeUser = makeFakeUser();
+    const fakeContent = new Content("new_message");
+    const newMessage = new Message(fakeUser, fakeContent)
+
+    expect(chat.addMessage(newMessage)).toBeTruthy();
   })
 })
