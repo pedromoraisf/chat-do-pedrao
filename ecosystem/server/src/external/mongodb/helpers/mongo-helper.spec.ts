d05dc889e39d45352764affc643fe6a63eefab11
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-22 20:17:17 -0300

    test: mongo helper

---
+++ ecosystem/server/src/external/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,20 @@
+import { MongoHelper as sut } from "./mongo-helper"
+
+describe("Mongo Helper", () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL ?? "")
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test("should reconnect if mongodb is down", async () => {
+    let accountCollection = await sut.getCollection("messages")
+    expect(accountCollection).toBeTruthy()
+
+    await sut.disconnect()
+    accountCollection = await sut.getCollection("messages")
+    expect(accountCollection).toBeTruthy()
+  })
+})
