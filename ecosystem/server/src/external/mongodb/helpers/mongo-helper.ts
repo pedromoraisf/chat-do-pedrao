2b4bcc4c1caa70846ccae53ce694f66b5f0e5c96
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-22 20:16:58 -0300

    feat: mongo helper

---
+++ ecosystem/server/src/external/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { MongoClient, Collection } from "mongodb"
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  url: null as unknown as string,
+
+  async connect(url: string): Promise<void> {
+    this.url = url
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+    this.client = null
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) await this.connect(this.url)
+    return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection
+
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  }
+}
