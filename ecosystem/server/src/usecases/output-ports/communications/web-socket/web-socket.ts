528f5fae5515b24fff528d11abdd2acc45dc7900
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-26 13:06:07 -0300

    feat: add initializeChatToListener draft

--- ecosystem/server/src/usecases/output-ports/communications/web-socket/web-socket.ts
+++ ecosystem/server/src/usecases/output-ports/communications/web-socket/web-socket.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Chat } from "@entities/chat"
+
 export interface WebSocket {
   sendBroadcastToAllListeners(payload: any): Promise<void>;
+  initializeChatToListener(chat: Chat): Promise<void>;
 }
