75f364bdeb75bc6665ababb35d559bf3e8b23521
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-26 13:07:33 -0300

    test: add initializeChatOnTheListener fake

--- ecosystem/server/src/usecases/output-ports/communications/web-socket/fake.ts
+++ ecosystem/server/src/usecases/output-ports/communications/web-socket/fake.ts
@@ -2,9 +2,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { WebSocket } from "@usecases/output-ports/communications/web-socket"
+import { Chat } from "@entities/chat"
 
 export class FakeWebSocket implements WebSocket {
   async sendBroadcastToAllListeners(payload: any): Promise<void> {
+    // 
+  }
+
+  async initializeChatOnTheListener(chat: Chat): Promise<void> {
     // 
   }
 }
