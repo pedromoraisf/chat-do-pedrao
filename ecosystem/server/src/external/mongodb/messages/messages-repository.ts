e508c466e3ec4b7264ab25dad8d48bacba1d5428
pedromoraisf <pedro.morais1997@gmail.com>
2021-03-26 11:39:53 -0300

    feat(external): add MessagesRepository

---
+++ ecosystem/server/src/external/mongodb/messages/messages-repository.ts
@@ -0,0 +1,44 @@
+import {
+  MessagesRepository,
+  RetrievMessagesResponse,
+  MessageToSave,
+  SaveMessageResponse
+} from "@usecases/output-ports/repositories/messages"
+import { MongoHelper } from "@external/mongodb/helpers"
+import { left, right } from "@shared/either"
+import { InfraError } from "@usecases/output-ports/errors";
+import { SavedMessage } from "@usecases/output-ports/repositories";
+
+export class MessagesRepo implements MessagesRepository {
+  public readonly COLLECTION_NAME = "messages";
+
+  async retrievMessages(): Promise<RetrievMessagesResponse> {
+    try {
+      const messagesCollection = await MongoHelper.getCollection(this.COLLECTION_NAME)
+      const result = await messagesCollection.find().toArray();
+      return right(result)
+    } catch {
+      return left(new InfraError())
+    }
+  }
+
+  async saveMessage(messageToSave: MessageToSave): Promise<SaveMessageResponse> {
+    const adaptedMessageToSave: SavedMessage = {
+      user: {
+        id: messageToSave.sender.id.value,
+        name: messageToSave.sender.name.value,
+        username: messageToSave.sender.username.value,
+        password: messageToSave.sender.password.value,
+      },
+      message: messageToSave.content.value
+    }
+
+    try {
+      const messagesCollection = await MongoHelper.getCollection(this.COLLECTION_NAME);
+      const result = await messagesCollection.insertOne(adaptedMessageToSave);
+      return right(result.ops[0]);
+    } catch {
+      return left(new InfraError());
+    }
+  }
+}
