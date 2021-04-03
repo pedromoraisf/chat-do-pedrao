00937fe0cc237b9a7f33b29e6d9ca2794467256c
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 14:55:34 -0300

    feat: add layer nested in error name

--- ecosystem/server/src/usecases/output-ports/errors/infra.ts
+++ ecosystem/server/src/usecases/output-ports/errors/infra.ts
@@ -1,6 +1,10 @@
+import { ARCHITECTURE_DISCLAIMER } from '@config/architecture-disclaimer';
+
+const { EXTERNAL } = ARCHITECTURE_DISCLAIMER;
+
 export class InfraError extends Error {
   constructor(message?: string) {
     super(`External infra error.\n${message}`);
-    this.name = 'InfraError';
+    this.name = `${EXTERNAL.errorPattern}/InfraError`;
   }
 }
