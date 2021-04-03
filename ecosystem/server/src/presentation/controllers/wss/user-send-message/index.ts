a9847348ef68f31b466aab5afce8c5506ba08836
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 15:11:01 -0300

    feat: add user send message controller

---
+++ ecosystem/server/src/presentation/controllers/wss/user-send-message/index.ts
@@ -0,0 +1,1 @@
+export * from '@presentation/controllers/wss/user-send-message/user-send-message';
---
+++ ecosystem/server/src/presentation/controllers/wss/user-send-message/user-send-message.ts
@@ -0,0 +1,43 @@
+import { UserSendMessage, MessagePack } from '@usecases/user-send-message';
+import { Controller, PayloadReceive, PayloadReturn } from '@presentation/controllers/wss/protocols';
+import { badRequest, ok } from '@presentation/controllers/wss/helpers/wss-helper';
+import { MissingParamError } from '@presentation/errors';
+import { mapError } from '@presentation/controllers/wss/helpers/error-mapper';
+
+export class UserSendMessageController implements Controller {
+  protected readonly userSendMessageUseCase: UserSendMessage;
+
+  constructor(userSendMessageUseCase: UserSendMessage) {
+    this.userSendMessageUseCase = userSendMessageUseCase;
+  }
+
+  async handle(payloadReceive: PayloadReceive): Promise<PayloadReturn> {
+    const normalizedPayloadReceive = this.normalizePayloadReceive(payloadReceive);
+    if (normalizedPayloadReceive.id === '' || normalizedPayloadReceive.user.id === '')
+      return badRequest(new MissingParamError(''));
+
+    try {
+      const userMessage = await this.userSendMessageUseCase.send(normalizedPayloadReceive);
+      return ok(userMessage.value);
+    } catch (e) {
+      const error = e.value;
+      return mapError(error.name)(error);
+    }
+  }
+
+  protected normalizePayloadReceive(payloadReceive: PayloadReceive): MessagePack {
+    const payloadReceiveTypes: MessagePack = {
+      id: '',
+      user: {
+        id: '',
+        name: '',
+        username: '',
+        password: ''
+      },
+      content: {
+        message: ''
+      }
+    };
+    return { ...payloadReceiveTypes, ...payloadReceive };
+  }
+}
