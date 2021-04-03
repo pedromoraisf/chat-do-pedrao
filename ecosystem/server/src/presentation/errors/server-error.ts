922da3dbd8a5948b97a30dd676ca126f0a130c61
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 14:09:42 -0300

    feat: add server error

--- ecosystem/server/src/presentation/errors/server-error.ts
+++ ecosystem/server/src/presentation/errors/server-error.ts
@@ -1,7 +1,11 @@
+import { ARCHITECTURE_DISCLAIMER } from '@config/architecture-disclaimer';
+
+const { PRESENTATION } = ARCHITECTURE_DISCLAIMER;
+
 export class ServerError extends Error {
   constructor(stack: string = '') {
     super('Internal server error');
-    this.name = 'ServerError';
+    this.name = `${PRESENTATION.errorPattern}/ServerError`;
     this.stack = stack;
   }
 }
