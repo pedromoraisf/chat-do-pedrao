81deb340f60ddc457efa15e050fde5eca019ce76
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 14:08:44 -0300

    feat: add architectural layers disclaimer

---
+++ ecosystem/server/src/config/architecture-disclaimer.ts
@@ -0,0 +1,14 @@
+export const ARCHITECTURE_DISCLAIMER = {
+  ENTITIES: {
+    errorPattern: 'EntitiesError'
+  },
+  USE_CASES: {
+    errorPattern: 'UseCasesError'
+  },
+  PRESENTATION: {
+    errorPattern: 'PresentationError'
+  },
+  EXTERNAL: {
+    errorPattern: 'ExternalError'
+  }
+};
