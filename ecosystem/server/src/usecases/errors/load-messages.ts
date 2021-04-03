b872a607aaaa3fa645bf19e62dc4fca58511ce15
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 14:12:10 -0300

    feat: add layer nested in error name

--- ecosystem/server/src/usecases/errors/load-messages.ts
+++ ecosystem/server/src/usecases/errors/load-messages.ts
@@ -1,6 +1,10 @@
+import { ARCHITECTURE_DISCLAIMER } from '@config/architecture-disclaimer';
+
+const { USE_CASES } = ARCHITECTURE_DISCLAIMER;
+
 export class LoadMessagesError extends Error {
   constructor(cause: string) {
     super(`Messages could not be loaded.\nCause: ${cause}`);
-    this.name = 'LoadMessagesError';
+    this.name = `${USE_CASES.errorPattern}/LoadMessagesError`;
   }
 }
