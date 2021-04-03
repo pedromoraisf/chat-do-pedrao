b21fe1ea38d0b840401cc2c8cdae0c0b3cc2f128
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-03 14:53:46 -0300

    feat: add error mapper

---
+++ ecosystem/server/src/presentation/controllers/wss/helpers/error-mapper.ts
@@ -0,0 +1,14 @@
+import { badRequest, serverError } from '@presentation/controllers/wss/helpers/wss-helper';
+import { ARCHITECTURE_DISCLAIMER } from '@config/architecture-disclaimer';
+
+const errorMapper = {
+  [ARCHITECTURE_DISCLAIMER.ENTITIES.errorPattern]: badRequest,
+  [ARCHITECTURE_DISCLAIMER.PRESENTATION.errorPattern]: badRequest,
+  [ARCHITECTURE_DISCLAIMER.USE_CASES.errorPattern]: badRequest,
+  [ARCHITECTURE_DISCLAIMER.EXTERNAL.errorPattern]: serverError
+};
+
+export const mapError = (errorName: string) => {
+  const layer = errorName.split('/')[0];
+  return errorMapper[layer];
+};
