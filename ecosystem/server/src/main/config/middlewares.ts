05fe6c1d4f8e0bbafaac77064ddcc16dc13befdc
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-04 15:34:41 -0300

    feat: remove unused features

--- ecosystem/server/src/main/config/middlewares.ts
+++ ecosystem/server/src/main/config/middlewares.ts
@@ -1,8 +1,6 @@
 import { Express } from 'express';
-import { bodyParser, cors, contentType } from '@main/middlewares';
+import { cors } from '@main/middlewares';
 
 export default (app: Express): void => {
-  app.use(bodyParser);
   app.use(cors);
-  app.use(contentType);
 };
--- ecosystem/server/src/main/middlewares/index.ts
+++ ecosystem/server/src/main/middlewares/index.ts
@@ -1,3 +1,1 @@
-export * from '@main/middlewares/body-parser';
-export * from '@main/middlewares/content-type';
 export * from '@main/middlewares/cors';
