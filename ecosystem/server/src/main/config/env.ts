7f1dce69baa98677448395920bd5037b38dcbb6f
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-05 20:38:30 -0300

    fix: default mongoUrl env

--- ecosystem/server/src/main/config/env.ts
+++ ecosystem/server/src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/chat-do-pedrao',
   port: process.env.PORT ?? 5050
 };
