daad52a010ee302cddd107b3e695493da5f5a15e
pedromoraisf <pedro.morais1997@gmail.com>
2021-04-05 22:34:21 -0300

    fix: send message to sender too

--- ecosystem/server/src/main/adapters/adapt-user-send-message.ts
+++ ecosystem/server/src/main/adapters/adapt-user-send-message.ts
@@ -4,6 +4,7 @@ import { Socket } from 'socket.io';
 export const adaptUserSendMessage = (socket: Socket, controller: Controller) => {
   return async (payload: any) => {
     const controllerReponse: PayloadReturn = await controller.handle(payload);
+    socket.emit('broadcast-user-send-message', controllerReponse);
     socket.broadcast.emit('broadcast-user-send-message', controllerReponse);
   };
 };
