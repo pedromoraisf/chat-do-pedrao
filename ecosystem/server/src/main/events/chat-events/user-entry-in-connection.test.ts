58fd06b0e73aa3b3d1bd57f4d69297341f9d570b
Pedro Morais <pedro.morais1997@gmail.com>
2021-07-19 16:45:57 -0300

    test: ensure concise integration test with socket + data persistence

--- ecosystem/server/src/main/events/chat-events/user-entry-in-connection.test.ts
+++ ecosystem/server/src/main/events/chat-events/user-entry-in-connection.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import chatEvents from '@main/events/chat-events';
 import { Server, Socket } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
@@ -6,6 +7,11 @@ import { MongoHelper } from '@external/mongodb/helpers';
 const { createServer } = require('http');
 
 const Client = require('socket.io-client');
+
+const insertFixtureInDataPersistence = async (messageToSave: any) => {
+  const messagesCollection = await MongoHelper.getCollection('messages');
+  return messagesCollection.insertOne(messageToSave);
+};
 
 describe('User Entry In Connection Integration Tests', () => {
   let io: { on: (arg0: string, arg1: (socket: any) => void) => void; close: () => void };
@@ -34,17 +40,33 @@ describe('User Entry In Connection Integration Tests', () => {
     MongoHelper.disconnect();
   });
 
-  test('functionality', (done) => {
+  test('should receive saved messages in messages repository', async (done) => {
+    await insertFixtureInDataPersistence({
+      user: {
+        id: 'any_id',
+        name: 'any_name',
+        username: 'any_username',
+        password: 'any_password'
+      },
+      message: 'any_message'
+    });
+
+    clientSocket.emit('user-entry-in-connection', true);
+
     clientSocket.on('response-user-entry-in-connection', (payload: any) => {
       expect(payload).toEqual({
         statusCode: 200,
         body: {
           id: 'global_chat',
-          messages: []
+          messages: [
+            {
+              sender: { id: 'any_id', name: 'any_name', username: 'any_username', password: 'any_password' },
+              content: { message: 'any_message' }
+            }
+          ]
         }
       });
       done();
     });
-    clientSocket.emit('user-entry-in-connection', true);
   });
 });
